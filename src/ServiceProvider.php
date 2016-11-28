e401724edba1a8788ddff9bcf97f3b22b15d1cc2
fisher <gooodfisher@126.com>
2016-11-28 12:07:08 +0800

    changeFileStructure

---
+++ src/ServiceProvider.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace LaravelSso\Server;
+
+use Illuminate\Foundation\Application as LaravelApplication;
+use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
+use Laravel\Lumen\Application as LumenApplication;
+
+class ServiceProvider extends LaravelServiceProvider
+{
+    /**
+     * 延迟加载.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Boot the provider.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->setupConfig();
+    }
+
+    /**
+     * Setup the config.
+     *
+     * @return void
+     */
+    protected function setupConfig()
+    {
+        $source = realpath(__DIR__.'/config.php');
+
+        if ($this->app instanceof LaravelApplication) {
+            if ($this->app->runningInConsole()) {
+                $this->publishes([
+                    $source => config_path('sso.php'),
+                ]);
+            }
+        } elseif ($this->app instanceof LumenApplication) {
+            $this->app->configure('sso');
+        }
+
+        $this->mergeConfigFrom($source, 'sso');
+    }
+
+    /**
+     * Register the provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton(['LaravelSso\\Server\\Server' => 'sso'], function ($app) {
+            return new Server(config('sso'));
+        });
+    }
+
+    /**
+     * 提供的服务
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['sso', 'LaravelSso\\Server\\Server'];
+    }
+}
